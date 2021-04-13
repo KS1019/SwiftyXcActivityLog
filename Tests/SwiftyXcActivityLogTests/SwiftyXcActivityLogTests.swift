dde447cf958ae6a3f90b73d0c3b6fe6c2af05e52
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-04-13 10:16:56 +0900

    Add Test case

--- Tests/SwiftyXcActivityLogTests/SwiftyXcActivityLogTests.swift
+++ Tests/SwiftyXcActivityLogTests/SwiftyXcActivityLogTests.swift
@@ -2,17 +2,23 @@ import XCTest
 @testable import SwiftyXcActivityLog
 
 final class SwiftyXcActivityLogTests: XCTestCase {
+    
+    // MARK: Testing for Extensions.swift
     func testUniqueElements() {
         let dupArray: [Int] = [1, 2, 3, 1, 2, 3]
         let noDupArray: [Int] = [1, 2, 3]
         XCTAssertEqual(dupArray.uniqueElements(noUnique: false), noDupArray)
     }
-    
-    func testLibraryDir() {
-        
+
+    func testReadByPattern() {
+        let testString = """
+0.01ms    /Users/testUser/Desktop/app-icon-resize-machine/app-icon-resize-machine/main.swift:19:16    setter configuration
+"""
+        XCTAssertTrue(testString.readByPattern())
     }
 
     static var allTests = [
-        ("testUniqueElements", testUniqueElements)
+        ("testUniqueElements", testUniqueElements),
+        ("testReadByPattern", testReadByPattern)
     ]
 }
